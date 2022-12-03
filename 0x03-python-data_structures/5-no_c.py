6d5a2fb4ca4f40dc82d1380b1e745426993f69e0
mekonnenageze <agezemekonnen34@gmail.com>
2022-12-02 23:33:49 -0800

    python-data_structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    result = []
-    for letter in my_string:
-        if letter != 'c' and letter != 'C':
-            result.append(letter)
-            return ''.join(result)
+    new_str = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_str += char
+            return new_str
