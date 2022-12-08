7b5f140b2f8e42beeaf4582d7a8866198e576386
Mekonnen <111089049+Mekonnenageze@users.noreply.github.com>
2022-12-08 12:01:22 +0300

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,6 @@
-##!/usr/bin/python3
-
+#!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dic = {}
-
-    for key in a_dictionary:
-        new_dic[key] = a_dictionary[key] * 2
-
-    return new_dic
+    """returns a new dictionary with"""
+    """all values multiplied by 2"""
+    new_dict = {x: (a_dictionary[x] * 2) for x in a_dictionary}
+    return new_dict
