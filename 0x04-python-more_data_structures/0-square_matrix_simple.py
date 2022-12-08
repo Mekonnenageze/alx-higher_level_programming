d335f61ce49140bf145042f62c7da8a828387c6c
Mekonnen <111089049+Mekonnenageze@users.noreply.github.com>
2022-12-08 11:47:17 +0300

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-    for row in range(len(new_matrix)):
-        new_matrix[row] = list(map(lambda x: x ** 2, new_matrix[row]))
-        return new_matrix
+    def square_cells(array):
+        result = []
+        for cell in array:
+            result.append(cell ** 2)
+        return result
+    return list(map(square_cells, matrix))
