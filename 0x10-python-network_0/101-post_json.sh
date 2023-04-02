216c9101ffd38b1d2716979f3f6d90a975394e32
Mekonnen <111089049+Mekonnenageze@users.noreply.github.com>
2023-04-02 16:14:32 +0300

    Update 101-post_json.sh
    Signed-off-by: Mekonnen <111089049+Mekonnenageze@users.noreply.github.com>

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# sends a JSON POST req to URL $!, display response body
-curl -sL -H "content-type:application/json"  -d @"$2" -X POST "$1"
+# Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -sX POST "$1" -H "Content-Type: application/json" -d @"$2"
 
